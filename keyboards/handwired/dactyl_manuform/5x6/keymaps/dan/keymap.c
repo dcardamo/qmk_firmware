4041cf05e360fddfd8e4081c5212fd9cd001c268
Dan Cardamore <dan@hld.ca>
2020-05-14 10:22:37 -0400

    make the space key act as a layer toggle when held

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/dan/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/dan/keymap.c
@@ -19,12 +19,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    [_QWERTY] = LAYOUT_5x6(
      NUMPAD, KC_1   , KC_2  , KC_3  , KC_4  , KC_5  ,                        KC_6  , KC_7  , KC_8  , KC_9  , KC_0  ,_______,
      KC_GRV , KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,                        KC_Y  , KC_U  , KC_I  , KC_O  , KC_P  ,KC_MINS,
-     KC_TAB , KC_A  , KC_S  , LT(RAISE, KC_D), LT(LOWER, KC_F), KC_G  ,      KC_H  , KC_J  , KC_K  , KC_L  ,KC_SCLN,KC_QUOT,
+     KC_TAB , KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,      KC_H  , KC_J  , KC_K  , KC_L  ,KC_SCLN,KC_QUOT,
      _______, KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                        KC_N  , KC_M  ,KC_COMM,KC_DOT ,KC_SLSH,KC_BSLASH,
                       KC_LBRC,KC_RBRC,                                                       KC_PLUS, KC_EQL,
-                         MT(MOD_LSFT, KC_ESC),KC_BSPC,                        KC_SPC, MT(MOD_RCTL, KC_ENT),  // enter when tapped, control when held
+                         MT(MOD_LSFT, KC_ESC),KC_BSPC,                        LT(_RAISE, KC_SPC), MT(MOD_RCTL, KC_ENT),  // enter when tapped, control when held
                 //   LT_RAISE_LPRN ,LT(FUNCNUM, KC_LBRC),                        KC_RBRC, LT_LOWER_RPRN,
-                   KC_LGUI ,KC_LALT,                                      _______, FUNCNUM,
+                   KC_LGUI ,KC_LALT,                                      FUNCNUM, RAISE,
                    _______ ,_______,                                      _______, _______
   ),
 
@@ -45,8 +45,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        //  Function keys plus Media                                            // Function keys, arrows, music
      _______,_______,_______,_______,_______,_______,                        _______,_______,_______,_______,_______,_______,
      _______,_______,_______,_______,_______,_______,                        KC_RBRC,_______,KC_NLCK,KC_INS ,KC_SLCK,KC_VOLU,
-     _______,_______,_______,_______,_______,_______,                        KC_LEFT,KC_DOWN,KC_UP,KC_RGHT,_______,KC_VOLD,
-     _______,_______,_______,_______,_______,_______,                        KC_MSTP,KC_MPRV,KC_MPLY,KC_MNXT,KC_VOLD,KC_MUTE,
+     _______,_______,LEFT_PAREN,KC_LBRACKET,LEFT_BRACE,_______,              KC_LEFT,KC_DOWN,KC_UP,KC_RGHT,_______,KC_VOLD,
+     _______,_______,RIGHT_PAREN,KC_RBRACKET,RIGHT_BRACE,_______,             KC_MSTP,KC_MPRV,KC_MPLY,KC_MNXT,KC_VOLD,KC_MUTE,
                                                _______,_______,            _______,GOLET  ,
                                                _______,_______,            _______,_______,
                                                _______,_______,            _______,_______,
@@ -67,8 +67,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [_FUNCNUM] = LAYOUT_5x6(
-     //  this side is for misc stuff                                         // This is the numpad
-     _______,_______,_______,_______,_______,_______,                        _______,_______,_______,_______,_______,_______,
+     KC_F12 , KC_F1 , KC_F2 , KC_F3 , KC_F4 , KC_F5 ,                        KC_F6  , KC_F7 , KC_F8 , KC_F9 ,KC_F10 ,KC_F11 ,
      KC_F12 , KC_F1 , KC_F2 , KC_F3 , KC_F4 , KC_F5 ,                        KC_F6  , KC_F7 , KC_F8 , KC_F9 ,KC_F10 ,KC_F11 ,
      _______,KC_1   ,KC_2   ,KC_3   ,KC_4   ,KC_5   ,                        KC_6   ,KC_7   ,KC_8   ,KC_9   ,KC_0   ,_______,
      _______,_______,_______,_______,_______,_______,                        _______,_______,_______,_______,_______,_______,
