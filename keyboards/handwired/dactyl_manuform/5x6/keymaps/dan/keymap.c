3b597c7c0cc4348bc4a9130960726b859e4cd0db
Dan Cardamore <dan@hld.ca>
2020-05-14 11:54:10 -0400

    space is just space

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/dan/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/dan/keymap.c
@@ -22,7 +22,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_TAB , KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,      KC_H  , KC_J  , KC_K  , KC_L  ,KC_SCLN,KC_QUOT,
      _______, KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                        KC_N  , KC_M  ,KC_COMM,KC_DOT ,KC_SLSH,KC_BSLASH,
                       KC_LBRC,KC_RBRC,                                                       KC_PLUS, KC_EQL,
-                         MT(MOD_LSFT, KC_ESC),KC_BSPC,                        LT(_RAISE, KC_SPC), MT(MOD_RCTL, KC_ENT),  // enter when tapped, control when held
+                         MT(MOD_LSFT, KC_ESC),KC_BSPC,                        KC_SPC, MT(MOD_RCTL, KC_ENT),  // enter when tapped, control when held
                 //   LT_RAISE_LPRN ,LT(FUNCNUM, KC_LBRC),                        KC_RBRC, LT_LOWER_RPRN,
                    KC_LGUI ,KC_LALT,                                      FUNCNUM, RAISE,
                    _______ ,_______,                                      _______, _______
