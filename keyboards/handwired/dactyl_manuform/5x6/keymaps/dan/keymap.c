fe56477f51538b29b4837b080de46eff2808659c
Dan Cardamore <dan@hld.ca>
2020-09-30 08:58:42 -0400

    make numpad harder to hit

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/dan/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/dan/keymap.c
@@ -17,10 +17,10 @@
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    [_QWERTY] = LAYOUT_5x6(
-     NUMPAD, KC_1   , KC_2  , KC_3  , KC_4  , KC_5  ,                        KC_6  , KC_7  , KC_8  , KC_9  , KC_0  ,_______,
+     _______, KC_1   , KC_2  , KC_3  , KC_4  , KC_5  ,                        KC_6  , KC_7  , KC_8  , KC_9  , KC_0  , KC_F12,
      KC_GRV , KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,                        KC_Y  , KC_U  , KC_I  , KC_O  , KC_P  ,KC_MINS,
      KC_TAB , KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,      KC_H  , KC_J  , KC_K  , KC_L  ,KC_SCLN,KC_QUOT,
-     _______, KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                        KC_N  , KC_M  ,KC_COMM,KC_DOT ,KC_SLSH,KC_BSLASH,
+     NUMPAD , KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                        KC_N  , KC_M  ,KC_COMM,KC_DOT ,KC_SLSH,KC_BSLASH,
                       KC_LBRC,KC_RBRC,                                                       KC_PLUS, KC_EQL,
                          MT(MOD_LSFT, KC_ESC),KC_BSPC,                        KC_SPC, MT(MOD_RCTL, KC_ENT),  // enter when tapped, control when held
                 //   LT_RAISE_LPRN ,LT(FUNCNUM, KC_LBRC),                        KC_RBRC, LT_LOWER_RPRN,
